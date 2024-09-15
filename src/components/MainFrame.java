b21ff49cf9bf8a98b5d88b43c3bf21eb2cf518e7
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-09-15 16:10:11 -0300

    Implement project TextPanel

--- src/components/MainFrame.java
+++ src/components/MainFrame.java
@@ -1,14 +1,25 @@
 package components;
 
+import components.panel.TextPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private final TextPanel textPanel;
+
     public MainFrame() {
         setTitle("Java GUI application");
         setSize((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.5),
                 (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.5));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        textPanel = new TextPanel(this);
+        add(textPanel, BorderLayout.CENTER);
+    }
+
+    public TextPanel getTextPanel() {
+        return textPanel;
     }
 }
