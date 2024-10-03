ec245c64232f7097d85a7b05de393878e1cd484e
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-10-03 20:36:09 -0300

    Update status text when manipulating files

--- src/components/menu/FileMenu.java
+++ src/components/menu/FileMenu.java
@@ -1,9 +1,13 @@
 package components.menu;
 
 import components.MainFrame;
-import java.io.*;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileMenu extends JMenu {
     public FileMenu(MainFrame mainFrame) {
@@ -25,35 +29,45 @@ public class FileMenu extends JMenu {
     }
 
     private void onOpenFileMenuItemSelected(MainFrame mainFrame) {
+        mainFrame.getStatusPanel().setStatusLabel("Abrindo arquivo...");
+
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Selecione um arquivo .txt");
 
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos de texto", "txt");
         fileChooser.setFileFilter(filter);
-            
+
         int result = fileChooser.showOpenDialog(mainFrame);
-        
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            
             StringBuilder content = new StringBuilder();
-            
+
             try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     content.append(line).append("\n");
                 }
             } catch (IOException e) {
-                JOptionPane.showMessageDialog(mainFrame, "Erro ao abrir o arquivo: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(
+                        mainFrame,
+                        "Erro ao abrir o arquivo: " + e.getMessage(),
+                        "Erro", JOptionPane.ERROR_MESSAGE
+                );
+                mainFrame.getStatusPanel().setStatusLabel("Erro ao abrir o arquivo!");
+
                 return;
             }
-            
+
             mainFrame.getTextPanel().setText(content.toString());
+            mainFrame.getStatusPanel().setStatusLabel("Arquivo aberto!");
+        } else {
+            mainFrame.getStatusPanel().setStatusLabel("Abertura de arquivo cancelada!");
         }
     }
 
     private void onCloseFileMenuItemSelected(MainFrame mainFrame) {
         mainFrame.getTextPanel().clearText();
+        mainFrame.getStatusPanel().setStatusLabel("Arquivo fechado!");
     }
 
     private void onExitMenuItemSelected() {
