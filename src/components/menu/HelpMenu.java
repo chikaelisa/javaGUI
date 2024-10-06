7fbaa24a3f0fdb50f6325fb5972eb5a4b6d94e65
Francisca Rosa <f.rosa96@hotmail.com>
2024-10-06 17:13:40 -0300

    feat: set status main frame message

--- src/components/menu/HelpMenu.java
+++ src/components/menu/HelpMenu.java
@@ -1,5 +1,6 @@
 package components.menu;
 
+import components.MainFrame;
 import components.dialog.HelpDialog;
 import utils.FileUtils;
 
@@ -8,34 +9,41 @@ import java.io.File;
 
 
 public class HelpMenu extends JMenu {
-    public HelpMenu() {
+    public HelpMenu(MainFrame mainFrame) {
         super("Ajuda");
 
         JMenuItem helpMenuItem = new JMenuItem("Ajuda");
-        helpMenuItem.addActionListener(e -> onHelpMenuItemSelected());
+        helpMenuItem.addActionListener(e -> onHelpMenuItemSelected(mainFrame));
 
         JMenuItem aboutMenuItem = new JMenuItem("Sobre");
-        aboutMenuItem.addActionListener(e -> onAboutMenuItemSelected());
+        aboutMenuItem.addActionListener(e -> onAboutMenuItemSelected(mainFrame));
 
         add(helpMenuItem);
         add(aboutMenuItem);
     }
 
-    private void onHelpMenuItemSelected() {
+    private void onHelpMenuItemSelected(MainFrame mainFrame) {
         File helpFile = new File("src/assets/help.txt");
         String content = FileUtils.readFile(helpFile);
         if (content != null) {
+            mainFrame.getStatusPanel().setStatusLabel("Ajuda");
             ImageIcon icon = new ImageIcon(getClass().getResource("/assets/fileMenu.png"));
             HelpDialog helpDialog = new HelpDialog("Ajuda", content, icon);
             helpDialog.show();
+        } else {
+            mainFrame.getStatusPanel().setStatusLabel("Erro ao abrir Ajuda");
         }
+
     }
 
-    private void onAboutMenuItemSelected() {
+    private void onAboutMenuItemSelected(MainFrame mainFrame) {
         File aboutFile = new File("src/assets/about.txt");
         String content = FileUtils.readFile(aboutFile);
         if (content != null) {
+            mainFrame.getStatusPanel().setStatusLabel("Sobre o projeto");
             FileUtils.displayContent(content, "Sobre este projeto");
+        } else {
+            mainFrame.getStatusPanel().setStatusLabel("Erro ao abrir Sobre o projeto");
         }
     }
 
